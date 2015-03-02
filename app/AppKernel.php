7c8ae7d10a247adf185554656d14e65845096c3e
Vincent <veysset.vincent@yahoo.fr>
2015-03-02 16:39:47 +0100

    Ajout bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            new Metinet\AppBundle\MetinetAppBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Metinet/AppBundle/MetinetAppBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Metinet\AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MetinetAppBundle extends Bundle
+{
+}
